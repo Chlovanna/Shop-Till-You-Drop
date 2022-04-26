03f385b3b3abb2133db1acbb9c3043ccf3631d5d
Chlovanna <ashdmt39@gmail.com>
2022-04-25 23:46:24 -0400

    update category-routes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
+# ORM Challenge: E-commerce Back End
 # Shop-Till-You-Drop
-ORM Challenge: E-commerce Back End
---
+++ routes/api/category-routes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+const { findOne } = require('../../models/Product');
+
+// The `/api/categories` endpoint
+ 
+// find all categories
+router.get('/', (req, res) => {
+  Category.findAll({
+    include:[
+      Product
+    ],
+      }) 
+ .then((catergories) => res.json(catergories))
+ .catch((err) => res.status(500).json(err));
+  // be sure to include its associated Products
+});
+
+// find one category by its `id` value
+  // be sure to include its associated Products
+router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  })
+  .then((catergory) => res.json(catergory))
+ .catch((err) => res.status(400).json(err));
+});
+
+// create a new category
+router.post('/', (req, res) => {
+ Category.create(req.body)
+ .then((category) => res.status(200).json(category))
+ .catch((err) => res.status(400).json(err)); 
+});
+
+// update a category by its `id` value
+router.put('/:id', (req, res) => {
+  Category.update(req.body)
+ .then((category) => res.status(200).json(category))
+ .catch((err) => res.status(400).json(err)); 
+  
+});
+
+// delete a category by its `id` value
+router.delete('/:id', (req, res) => {
+  Category.destroy(req.body)
+ .then((category) => res.status(200).json(category))
+ .catch((err) => res.status(400).json(err));  
+});
+
+module.exports = router;
